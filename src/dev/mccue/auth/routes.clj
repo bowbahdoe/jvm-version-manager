fd51e4d7d452eaf7f6035c79f28d05fa9e798f24
Ethan McCue <ethan@mccue.dev>
2026-05-22 07:12:16 -0400

    Update routes.clj

--- src/dev/mccue/auth/routes.clj
+++ src/dev/mccue/auth/routes.clj
@@ -64,13 +64,13 @@
             [:aside {:class (classes ["w-60 border-r bg-white flex flex-col"])}
              [:nav {:class (classes ["flex-1 px-3 space-y-1"])}
               [:a {:class (classes ["flex items-center gap-3 rounded-lg px-4 py-2 text-gray-700 hover:bg-gray-100"])}
-               "ABC"]
+               "Ask"]
               [:a {:class (classes ["flex items-center gap-3 rounded-lg px-4 py-2 text-gray-700 hover:bg-gray-100"])}
-               "DEF"]
+               "Answer"]
               [:a {:class (classes ["flex items-center gap-3 rounded-lg px-4 py-2 text-gray-700 hover:bg-gray-100"])}
-               "DEF"]
+               "Search"]
               [:a {:class (classes ["flex items-center gap-3 rounded-lg px-4 py-2 text-gray-700 hover:bg-gray-100"])}
-               "DEF"]]
+               "Chat"]]
              #_[:nav {:class (classes ["flex-1 px-3 space-y-1"])}]]
 
             [:main {:class (classes ["flex-1 overflow-y-auto"])}
