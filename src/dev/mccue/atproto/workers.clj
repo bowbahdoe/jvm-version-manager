b35fbfc15bce57f10452414c394afc0e9de3da5e
Ethan McCue <ethan@mccue.dev>
2026-05-29 23:56:36 -0400

    Update workers.clj

--- src/dev/mccue/atproto/workers.clj
+++ src/dev/mccue/atproto/workers.clj
@@ -33,69 +33,73 @@
 
 (defn handle-module-create!
   [db payload]
-  (let [publisher-did       (get-in payload [:event :did])
-        blob-link           (get-in payload [:event :commit :record :artifact :ref :$link])
-        blob-response       (http/get
-                              (str @service-endpoint get-blob-xrpc
-                                   "?cid=" blob-link
-                                   "&did=" publisher-did)
-                              {:as :byte-array})
-        module-info         (artifact/module-info-from-archive-bytes
-                              (:body blob-response))
-        module-name         (:name module-info)
-        module-version      (:version module-info)
-        rkey                (get-in payload [:event :commit :rkey])
-        [rkey-module-name
-         rkey-module-version] (string/split rkey #":")]
-    (cond
-      (not= rkey-module-name module-name)
-      (log/warn "Published module name does not match name in rkey. rkey="
-                rkey
-                ", module-name="
-                module-name)
+  (let [publisher-did (get-in payload [:event :did])]
+    (if-not (get-in payload [:event :commit :record :indexMe])
+      (log/info "indexMe is false. Skipping indexing module." publisher-did)
+      (let [blob-link           (get-in payload [:event :commit :record :artifact :ref :$link])
+            blob-response       (http/get
+                                  (str @service-endpoint get-blob-xrpc
+                                       "?cid=" blob-link
+                                       "&did=" publisher-did)
+                                  {:as :byte-array})
+            module-info         (artifact/module-info-from-archive-bytes
+                                  (:body blob-response))
+            module-name         (:name module-info)
+            module-version      (:version module-info)
+            rkey                (get-in payload [:event :commit :rkey])
+            [rkey-module-name
+             rkey-module-version] (string/split rkey #":")]
+        (cond
+          (not= rkey-module-name module-name)
+          (log/warn "Published module name does not match name in rkey. rkey="
+                    rkey
+                    ", module-name="
+                    module-name)
 
-      (and rkey-module-version
-           (not= module-version rkey-module-version))
-      (if module-version
-        (log/warn "Published module version does not match version in rkey. rkey="
-                  rkey
-                  ", version in module-info=" module-version)
-        (log/warn "Published module version does not match version in rkey. rkey="
-                  rkey
-                  ", version in module-info=null"))
+          (and rkey-module-version
+               (not= module-version rkey-module-version))
+          (if module-version
+            (log/warn "Published module version does not match version in rkey. rkey="
+                      rkey
+                      ", version in module-info=" module-version)
+            (log/warn "Published module version does not match version in rkey. rkey="
+                      rkey
+                      ", version in module-info=null"))
 
-      :else
-      (do
 
-        (jdbc/execute! db (sql/format
-                            {:insert-into :repository.module_provider
-                             :columns     [:atproto_did :module_name]
-                             :values      [[publisher-did (:name module-info)]]
-                             :on-conflict []
-                             :do-nothing  true}))
-        (let [providers          (jdbc/execute! db (sql/format
-                                                    {:select [:atproto_did]
-                                                     :from   :repository.module_provider
-                                                     :where  [:= :module_name (:name module-info)]}))
-              {:keys [accessJwt]} (-> (http/post
-                                        (str @service-endpoint create-session-xrpc)
-                                        {:headers {"Content-Type" "application/json"}
-                                         :body (json/generate-string {"identifier" (System/getenv "ATPROTO_INDEXER_HANDLE")
-                                                                      "password"   (System/getenv "ATPROTO_INDEXER_APP_PASSWORD")})})
-                                      (:body)
-                                      (json/parse-string-strict keyword))]
-          (http/post
-            (str @service-endpoint put-record-xrpc)
-            {:body (json/generate-string
-                     {:repo       @did
-                      :rkey       (:name module-info)
-                      :collection "dev.mccue.jvm.index"
-                      :record     {"$type" "dev.mccue.jvm.index"
-                                   "createdAt" (str (OffsetDateTime/now))
-                                   "providers" (for [provider providers]
-                                                 {"did" (:module_provider/atproto_did provider)})}})
-             :headers {"Authorization" (str "Bearer " accessJwt)
-                       "Content-Type"  "application/json"}}))))))
+
+          :else
+          (do
+
+            (jdbc/execute! db (sql/format
+                                {:insert-into :repository.module_provider
+                                 :columns     [:atproto_did :module_name]
+                                 :values      [[publisher-did (:name module-info)]]
+                                 :on-conflict []
+                                 :do-nothing  true}))
+            (let [providers          (jdbc/execute! db (sql/format
+                                                        {:select [:atproto_did]
+                                                         :from   :repository.module_provider
+                                                         :where  [:= :module_name (:name module-info)]}))
+                  {:keys [accessJwt]} (-> (http/post
+                                            (str @service-endpoint create-session-xrpc)
+                                            {:headers {"Content-Type" "application/json"}
+                                             :body (json/generate-string {"identifier" (System/getenv "ATPROTO_INDEXER_HANDLE")
+                                                                          "password"   (System/getenv "ATPROTO_INDEXER_APP_PASSWORD")})})
+                                          (:body)
+                                          (json/parse-string-strict keyword))]
+              (http/post
+                (str @service-endpoint put-record-xrpc)
+                {:body (json/generate-string
+                         {:repo       @did
+                          :rkey       (:name module-info)
+                          :collection "dev.mccue.jvm.index"
+                          :record     {"$type" "dev.mccue.jvm.index"
+                                       "createdAt" (str (OffsetDateTime/now))
+                                       "providers" (for [provider providers]
+                                                     {"did" (:module_provider/atproto_did provider)})}})
+                 :headers {"Authorization" (str "Bearer " accessJwt)
+                           "Content-Type"  "application/json"}}))))))))
 
 (comment
   (handle-module-create! nil nil))
@@ -118,8 +122,11 @@
       (and (= event-kind "commit")
            (= (get-in payload [:event :commit :record :$type])
               "dev.mccue.jvm.module")
-           (= (get-in payload [:event :commit :operation])
-              "create"))
+           (or (= (get-in payload [:event :commit :operation])
+                  "create")
+               ;; TODO: updates and deletes very likely should have different handling
+               (= (get-in payload [:event :commit :operation])
+                  "update")))
       (handle-module-create! db payload)
 
       :else
